3cba05ee1f51bbf6da90658c8fffe2d8eae22991
abdelrouf-ali <abdelrouf65@gmail.com>
2023-07-21 01:40:19 +0300

    Initial commit

---
+++ src/site/css/blogpost.11ty.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+
+const fileName = "blogpost.css";
+
+
+module.exports = class {
+  async data () {
+    const rawFilepath = path.join(__dirname, `../_includes/postcss/${fileName}`);
+    return {
+      permalink: `css/${fileName}`,
+      rawFilepath,
+      rawCss: await fs.readFileSync(rawFilepath)
+    };
+  };
+
+  async render ({ rawCss, rawFilepath }) {
+    return await postcss([
+      require('postcss-import'),
+      require('precss'),
+      require('autoprefixer')
+    ])
+    .process(rawCss, { from: rawFilepath })
+    .then(result => result.css);
+  };
+}
